62560da5ee6864d46ec0c58557f55caf88970217
Sin-Fong Lyu <kingispeak@gmail.com>
2022-09-07 13:18:05 +0800

    Ceate a multiple-page application with vue 3

---
+++ resources/views/pages/about.blade.php
@@ -0,0 +1,13 @@
+@extends('layouts.app')
+
+@section('title', 'About')
+
+@section('content')
+    <h1>About</h1>
+    <about />
+@endsection
+
+@push('scripts')
+    @vite('resources/js/pages/about.js')
+@endpush
+
